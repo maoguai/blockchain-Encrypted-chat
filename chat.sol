beb14c90f6983fdd2bbaa7532cc7e4cc956d08ab
Ding zh <810990206@qq.com>
2019-03-15 18:17:32 +0800

    Update chat.sol

--- chat.sol
+++ chat.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.7;
 
 contract chat {
     
-    event Keyagrement1(address se,address rec,string p,string ka)
-    event Keyagrement2(address re,address sen,string kb)
-    event ReceiveMessage(address from,address to,string msgtext)
+    event Keyagrement1(address se,address rec,string p,string ka);
+    event Keyagrement2(address re,address sen,string kb);
+    event ReceiveMessage(address from,address to,string msgtext);
     //秘钥协商——sender向receiver发起好友申请
     function keyagrement1(address receiver,string prime1,string key1){
         Keyagrement1(msg.sender,receiver,prime1,key1);
@@ -17,7 +17,7 @@ contract chat {
     
     //聊天
     function sendMessage(address msgreceiver,string crypted){
-        ReceiveMessage(msg.sender,msgreceiver,crypted)
+        ReceiveMessage(msg.sender,msgreceiver,crypted);
     }
     
 }
